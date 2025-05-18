070bfa31f2848e9a5f9b276f687c3b7449d182fa
Noel Kiprono Langat <noellangat28@gmail.com>
2025-05-18 19:35:14 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,28 +1,17 @@
-
--- Initial Complex Query (Before Optimization)
+-- Initial unoptimized query to retrieve bookings with related user, property, and payment details
+EXPLAIN ANALYZE
 SELECT 
     b.id AS booking_id,
+    u.id AS user_id,
     u.name AS user_name,
-    u.email AS user_email,
+    p.id AS property_id,
     p.title AS property_title,
-    p.location,
+    pay.id AS payment_id,
     pay.amount,
     pay.status
 FROM bookings b
 JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
-JOIN payments pay ON b.id = pay.booking_id;
-
--- Optimized Query (After Indexing and Refactoring)
-SELECT 
-    b.id AS booking_id,
-    u.name AS user_name,
-    u.email AS user_email,
-    p.title AS property_title,
-    p.location,
-    pay.amount,
-    pay.status
-FROM bookings b
-INNER JOIN users u ON u.id = b.user_id
-INNER JOIN properties p ON p.id = b.property_id
-INNER JOIN payments pay ON pay.booking_id = b.id;
+LEFT JOIN payments pay ON b.id = pay.booking_id
+WHERE b.start_date >= '2024-01-01'
+AND b.status = 'confirmed';
